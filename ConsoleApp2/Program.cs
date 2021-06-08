4ca97846bb119fa55d50adb16b08ef19506def66
Snow <snowasdev@gmail.com>
2021-06-08 15:34:17 +0200

    take it

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "snow";
+            string password = "password";
+            using (StreamWriter writer = new StreamWriter(@"D:\login.storage"))
+            {
+                writer.WriteLine(username);
+                writer.WriteLine(password);
+            }
+            if (File.Exists(@"D:\login.storage"))
+            {
+                int counter = 0;
+                string line;
+
+                System.IO.StreamReader file = new System.IO.StreamReader(@"D:\login.storage");
+                while ((line = file.ReadLine()) != null)
+                {
+                    switch (counter)
+                    {
+                        case 0:
+                            Console.WriteLine("Username: "+line);
+                            break;
+                        case 1:
+                            Console.WriteLine("Password: "+line);
+                            break;
+                        default:
+                            Console.WriteLine("not defined :"+line);
+                            break;
+                    }
+                    counter++;
+                }
+
+                file.Close();
+            }
+
+            Console.Read();
+        }
+    }
+}
